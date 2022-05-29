d9015c2d223bc9b99e8828d5d60fa07461ccb4a9
Fabri210 <fabrizio.alberio@gmail.com>
2022-05-29 15:34:13 +0200

    added AppsGroup and AppRow classes

--- src/apps_view.py
+++ src/apps_view.py
@@ -7,6 +7,7 @@ class AppsView(Gtk.Box):
     __gtype_name__ = 'AppsView'
 
     new_file_button = Gtk.Template.Child('new_file_button')
+    main_box = Gtk.Template.Child('main_box')
 
     DEFAULT_FOLDERS = [
         '~/.local/share/applications/',
@@ -27,4 +28,14 @@ class AppsView(Gtk.Box):
         for folder in self.folders:
             folder.get_files()
 
-        
+class AppsGroup(Adw.PreferencesGroup):
+    __gtype_name__ = 'AppsGroup'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class AppRow(Adw.ActionRow):
+    __gtype_name__ = 'AppRow'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
