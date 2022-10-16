3e99010c55effd117b557289dfa4a03dd15d85da
Fabri210 <fabrizio.alberio@gmail.com>
2022-10-16 10:58:54 +0200

    Load system apps from mounted host filesystem

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ USER_DATA = Path.home() / '.local/share'
 USER_APPS = USER_DATA / 'applications'
 USER_ICONS = USER_DATA / 'icons'
 
-SYSTEM_DATA = Path('/usr/share')
+SYSTEM_DATA = Path('/run/host/usr/share')
 SYSTEM_APPS = SYSTEM_DATA / 'applications'
 SYSTEM_ICONS = SYSTEM_DATA / 'icons'
 
@@ -19,6 +19,8 @@ FLATPAK_SYSTEM = Path('/var/lib/flatpak')
 FLATPAK_SYSTEM_APPS = FLATPAK_SYSTEM / 'exports/share/applications'
 FLATPAK_SYSTEM_ICONS = FLATPAK_SYSTEM / 'exports/share/icons'
 
+LOCALE_DIR = SYSTEM_DATA / 'locale'
+
 
 def escape_xml(string: str) -> str:
     return escape(string or '')
@@ -28,9 +30,9 @@ def set_icon_from_name(icon: Gtk.Image, icon_name: str) -> Gtk.Image:
 
     icon.set_from_icon_name('application-x-executable')
     if icon_name != None:
-        if Path(icon_name).is_file():
-            icon.set_from_file(icon_name)
-        elif theme.has_icon(icon_name) or theme.has_icon(f'{icon_name}-symbolic'): # Have to specify symbolic icons because they still show up
+        if theme.has_icon(icon_name) or theme.has_icon(f'{icon_name}-symbolic'): # Have to specify symbolic icons because they still show up
             icon.set_from_icon_name(icon_name)
+        elif Path(icon_name).is_file():
+            icon.set_from_file(icon_name)
 
     return icon
