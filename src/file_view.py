a0f4ff550515ee8fba38d50d620d73720c469fe6
fabrialberio <->
2024-02-29 08:51:23 +0100

    Made fields removable

--- src/file_view.py
+++ src/file_view.py
@@ -37,7 +37,7 @@ class BoolRow(Adw.ActionRow):
         self.switch.set_active(file[field])
 
         def update_field(self, value: bool):
-            file.set(field, value, emit=False)
+            file.set(field, value)
 
         def update_state(file: DesktopFile, field_: Field[bool], value: bool):
             if field_ == field and value != self.switch.get_active():
@@ -49,42 +49,88 @@ class BoolRow(Adw.ActionRow):
 class StringRow(Adw.EntryRow):
     __gtype_name__ = 'StringRow'
 
-    def __init__(self) -> None:
-        super().__init__()
+    file: DesktopFile
+    field: Field[str]
+    remove_button: Gtk.Button
+    _removable: bool = True
+
+    @GObject.Property(type=bool, default=True)
+    def removable(self) -> bool: # type: ignore
+        return self._removable
     
+    @removable.setter
+    def removable(self, value: bool):
+        self._removable = value
+
+    def __init__(self, removable: bool=True) -> None:
+        self._removable = removable
+
+        super().__init__()
+
+        self.remove_button = Gtk.Button(
+            valign=Gtk.Align.CENTER,
+            visible=False,
+            icon_name='edit-delete-symbolic',
+            tooltip_text=_('Remove field'),
+        )
+        self.remove_button.add_css_class('flat')
+        self.remove_button.connect('clicked', lambda w: self.remove_field())
+        self.connect('changed', lambda w: self.update_remove_button_visible())
+        self.add_suffix(self.remove_button)
+
     def set_field(self, file: DesktopFile, field: Field[str]) -> None:
+        self.file = file
+        self.field = field
+
         self.set_title(field.key)
         self.set_text(file.get(field, ''))
 
         def update_field(widget: StringRow):
-            file.set(field, self.get_text(), emit=False)
-
-        def update_text(file: DesktopFile, field_: Field[str], value: str):
-            if field_ == field and value != self.get_text():
-                self.set_text(value)
+            file.set(self.field, self.get_text())
 
         self.connect('changed', update_field)
-        file.connect('field-set', update_text)
+
+    def remove_field(self):
+        self.set_visible(False)
+        self.file.remove(self.field)
+
+    def update_remove_button_visible(self):
+        if self.removable:
+            self.remove_button.set_visible(not self.get_text())
+        else:
+            self.remove_button.set_visible(False)
+
 
 class LocaleButton(Gtk.MenuButton):
     __gtype_name__ = 'LocaleSelectButton'
 
+    popover: Gtk.Popover
     selected: Optional[str] = None
 
-    def __init__(self, file: DesktopFile, field: LocaleField[str]) -> None:
+    def __init__(self) -> None:
         button_content = Adw.ButtonContent(
             icon_name='preferences-desktop-locale-symbolic'
         )
-        popover = Gtk.Popover()
-        popover.add_css_class('menu')
+        self.popover = Gtk.Popover()
+        self.popover.add_css_class('menu')
 
         super().__init__(
-            valign=Gtk.Align.CENTER,
             child=button_content,
-            popover=popover,
+            valign=Gtk.Align.CENTER,
+            popover=self.popover,
+            tooltip_text=_('Select locale'),
         )
         self.add_css_class('flat')
 
+        def update_label(widget: LocaleButton, selected: Optional[str]):
+            if selected is not None:
+                button_content.set_label(selected)
+            else:
+                button_content.set_label('')
+
+        self.connect('changed', update_label)
+
+    def set_field(self, file: DesktopFile, field: LocaleField):
         UNLOCALIZED_STR = _('(Unlocalized)')
         items = [UNLOCALIZED_STR] + file.locales(field)
 
@@ -120,7 +166,7 @@ class LocaleButton(Gtk.MenuButton):
 
         def item_selected(list_view: Gtk.ListView, index: int):
             self.set_selected(items[index] if index > 0 else None)
-            popover.popdown()
+            self.popover.popdown()
 
         list_view = Gtk.ListView(
             single_click_activate=True,
@@ -129,7 +175,7 @@ class LocaleButton(Gtk.MenuButton):
         )
         list_view.connect('activate', item_selected)
 
-        popover.set_child(Gtk.ScrolledWindow(
+        self.popover.set_child(Gtk.ScrolledWindow(
             hscrollbar_policy=Gtk.PolicyType.NEVER,
             max_content_height=400,
             propagate_natural_width=True,
@@ -137,56 +183,55 @@ class LocaleButton(Gtk.MenuButton):
             child=list_view
         ))
 
-        def update_label(widget: LocaleButton, selected: Optional[str]):
-            if selected is not None:
-                button_content.set_label(selected)
-            else:
-                button_content.set_label('')
-
-        self.connect('changed', update_label)
-
     def set_selected(self, locale: Optional[str]):
         self.selected = locale
         self.emit('changed', locale)
 
 GObject.signal_new('changed', LocaleButton, GObject.SignalFlags.RUN_FIRST, None, (GObject.TYPE_PYOBJECT,))
 
-class LocaleStringRow(Adw.EntryRow):
+class LocaleStringRow(StringRow):
     __gtype_name__ = 'LocaleStringRow'
 
-    file: DesktopFile
-    field: LocaleField[str]
     locale: Optional[str] = None
+    locale_field: LocaleField[str]
+    locale_select: LocaleButton
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, removable: bool=True) -> None:
+        super().__init__(removable)
 
-    def set_field(self, file: DesktopFile, field: LocaleField[str]) -> None:
-        self.set_title(field.key)
+        self.locale_select = LocaleButton()
+        self.locale_select.connect('changed', lambda w, l: self.set_locale(l))
+        self.add_suffix(self.locale_select)
 
-        self.file = file
-        self.field = LocaleField(field.group, field.key, str)
+    def set_field(self, file: DesktopFile, field: LocaleField) -> None:
+        super().set_field(file, field.localize(None))
+        self.locale_select.set_field(file, field)
 
-        locale_select = LocaleButton(file, field)
-        locale_select.connect('changed', lambda w, l: self.set_locale(l))
-        self.add_suffix(locale_select)
-
+        self.locale_field = LocaleField(field.group, field.key, str)
         self.set_locale(None)
-
-        def update_field(widget: StringRow):
-            file.set(self.field.localize(self.locale), self.get_text(), emit=False)
-
-        def update_text(file: DesktopFile, field_: Field[str], value: str):
-            if field_ == self.field.localize(self.locale) and value != self.get_text():
-                self.set_text(value)
-
-        self.connect('changed', update_field)
-        file.connect('field-set', update_text)
 
     def set_locale(self, locale: Optional[str]):
         self.locale = locale
-        self.set_text(self.file.get(self.field.localize(locale), ''))
+        self.field = self.locale_field.localize(locale)
+        self.set_text(self.file.get(self.field, ''))
+        self.update_remove_button_visible()
 
+    def remove_field(self):
+        self.file.remove(self.field)
+
+        if self.locale is None and not self.file.locales(self.locale_field):
+            self.set_visible(False)
+        else:
+            self.locale_select.set_selected(None)
+            self.locale_select.set_field(self.file, self.locale_field)
+
+    def update_remove_button_visible(self):
+        if self.removable and self.locale is not None:
+            self.remove_button.set_visible(not self.get_text())
+        elif self.removable and self.locale is None and not self.file.locales(self.locale_field):
+            self.remove_button.set_visible(not self.get_text())
+        else:
+            self.remove_button.set_visible(False)
 
 @Gtk.Template(resource_path='/io/github/fabrialberio/pinapp/file_view.ui')
 class FileView(Adw.BreakpointBin):
@@ -201,6 +246,7 @@ class FileView(Adw.BreakpointBin):
     comment_row: LocaleStringRow = Gtk.Template.Child()
     icon_row: StringRow = Gtk.Template.Child()
     values_group: Adw.PreferencesGroup = Gtk.Template.Child()
+    add_field_button: Gtk.Button = Gtk.Template.Child()
 
     def __init__(self, file: DesktopFile):
         super().__init__()
@@ -214,7 +260,6 @@ class FileView(Adw.BreakpointBin):
         self.comment_row.set_field(self.file, DesktopEntry.COMMENT)
 
         def update_icon(row: StringRow):
-            print('update_icon', self.icon_row.get_text())
             set_icon_from_name(self.icon, self.icon_row.get_text())
 
         self.icon_row.connect('changed', update_icon)
@@ -224,7 +269,7 @@ class FileView(Adw.BreakpointBin):
                 continue
 
             if field in DesktopEntry.fields:
-                field = next(f for f in DesktopEntry.fields if f.key == field.key)
+                field = next(f for f in DesktopEntry.fields if f == field)
 
             if field._type == bool:
                 row = BoolRow()
@@ -234,12 +279,21 @@ class FileView(Adw.BreakpointBin):
                 row.set_field(self.file, LocaleField(field.group, field.key, str))
             else:
                 row = StringRow()
+
+                if field == DesktopEntry.TYPE:
+                    row.removable = False
+
                 row.set_field(self.file, Field(field.group, field.key, str))
 
             self.values_group.add(row)
 
-    '''
-    def _add_key(self, is_bool=False, is_localized=False):
+        def on_add_field(button: Gtk.Button):
+            self.show_add_field_dialog()
+
+        self.add_field_button.connect('clicked', on_add_field)
+
+    def show_add_field_dialog(self):
+        return
         builder = Gtk.Builder.new_from_resource('/io/github/fabrialberio/pinapp/file_page_dialogs.ui')
 
         add_key_dialog = builder.get_object('add_key_dialog')
@@ -273,10 +327,6 @@ class FileView(Adw.BreakpointBin):
         add_key_dialog.connect('response', callback)
         add_key_dialog.set_transient_for(self.get_root())
         add_key_dialog.present()
-    '''
 
-    def add_row(self):
-        ...
-
-    def remove_row(self, field: Field):
+    def remove_field(self, field: Field):
         ...
