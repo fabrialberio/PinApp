ed34386034aee1806108f40bbbb9c408384ee753
fabrialberio <->
2024-08-09 12:37:13 +0200

    improved create_gfile_checked

--- src/file_pool.py
+++ src/file_pool.py
@@ -7,20 +7,18 @@ from .config import *
 
 
 def create_gfile_checked_samedir(path: str) -> Gio.File:
-    index = 0
-    
-    while True:
+    for index in range(0, 999999):
         suffix = path.split('.')[-1]
-        new_path = f'{path.removesuffix(suffix)}{f'-{index}' if index > 0 else ''}{suffix}'
+        new_path = f'{path.removesuffix('.' + suffix)}{f'-{index}' if index > 0 else ''}.{suffix}'
         new_gfile = Gio.File.new_for_path(new_path)
 
         try:
             new_gfile.create(Gio.FileCreateFlags.NONE)
-            break
+            return new_gfile
         except GLib.GError:
-            index += 1
-    
-    return new_gfile
+            continue
+
+    raise IOError('Failed to create gfile after trying all indexes.')
 
 def create_gfile_checked(basename: str, parent: str) -> Gio.File:
     return create_gfile_checked_samedir(f'{parent}/{basename}')
