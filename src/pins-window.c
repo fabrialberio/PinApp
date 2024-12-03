ef860bdf70c658d6f4ff30d58a45350facafd8cc
fabrialberio <->
2024-12-03 22:48:35 +0100

    fix: Remove testing code

--- src/pins-window.c
+++ src/pins-window.c
@@ -50,24 +50,5 @@ pins_window_class_init (PinsWindowClass *klass)
 static void
 pins_window_init (PinsWindow *self)
 {
-    PinsAppRow *row;
-    PinsDesktopFile *desktop_file;
-    GKeyFile *key_file;
-    gchar *name;
-
     gtk_widget_init_template (GTK_WIDGET (self));
-
-    row = pins_app_row_new ();
-
-    desktop_file = pins_desktop_file_new_from_file (
-        g_file_new_for_path (
-            "/home/fabri/Progetti/App "
-            "GNOME/Pins/data/io.github.fabrialberio.pinapp.desktop.in"),
-        NULL);
-
-    g_warning ("user_path");
-
-    pins_app_row_set_desktop_file (row, desktop_file);
-
-    adw_preferences_group_add (self->box, GTK_WIDGET (row));
 }
