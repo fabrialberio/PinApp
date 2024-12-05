d1fd9a9d9a030a134c83733cf41a7975062c9a79
fabrialberio <->
2024-12-05 08:16:09 +0100

    fix: Wrong method name

--- src/pins-directories.h
+++ src/pins-directories.h
@@ -23,7 +23,7 @@
 G_BEGIN_DECLS
 
 gchar *pins_user_app_path (void);
-gchar **pins_get_user_app_paths (void);
+gchar **pins_system_app_paths (void);
 
 void pins_icon_theme_inject_search_paths (GtkIconTheme *theme);
 
