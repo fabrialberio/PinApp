c31c8be00588a1784a9c5d1bf6a4d3cc8d5ebb86
fabrialberio <->
2024-12-05 10:55:00 +0100

    feature: Start implementing search behaviour

--- src/pins-window.c
+++ src/pins-window.c
@@ -31,7 +31,10 @@ struct _PinsWindow
     /* Template widgets */
     GtkButton *new_file_button;
     GtkToggleButton *search_button;
+    GtkSearchBar *search_bar;
+    GtkSearchEntry *search_entry;
     PinsAppList *app_list;
+    PinsAppList *search_list;
 };
 
 G_DEFINE_FINAL_TYPE (PinsWindow, pins_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -39,7 +42,16 @@ G_DEFINE_FINAL_TYPE (PinsWindow, pins_window, ADW_TYPE_APPLICATION_WINDOW)
 static void
 pins_window_dispose (GObject *object)
 {
+    PinsWindow *self = PINS_WINDOW (object);
+
     gtk_widget_dispose_template (GTK_WIDGET (object), PINS_TYPE_WINDOW);
+
+    g_clear_object (&self->new_file_button);
+    g_clear_object (&self->search_button);
+    g_clear_object (&self->search_bar);
+    g_clear_object (&self->search_entry);
+    g_clear_object (&self->app_list);
+    g_clear_object (&self->search_list);
 
     G_OBJECT_CLASS (pins_window_parent_class)->dispose (object);
 }
@@ -60,14 +72,20 @@ pins_window_class_init (PinsWindowClass *klass)
                                           new_file_button);
     gtk_widget_class_bind_template_child (widget_class, PinsWindow,
                                           search_button);
+    gtk_widget_class_bind_template_child (widget_class, PinsWindow,
+                                          search_bar);
+    gtk_widget_class_bind_template_child (widget_class, PinsWindow,
+                                          search_entry);
     gtk_widget_class_bind_template_child (widget_class, PinsWindow, app_list);
+    gtk_widget_class_bind_template_child (widget_class, PinsWindow,
+                                          search_list);
 }
 
 static void
 pins_window_init (PinsWindow *self)
 {
     GtkIconTheme *theme;
-    GListModel *app_iterator;
+    PinsAppIterator *app_iterator;
 
     gtk_widget_init_template (GTK_WIDGET (self));
 
@@ -77,7 +95,11 @@ pins_window_init (PinsWindow *self)
     // This is noticeably slow
     pins_icon_theme_inject_search_paths (theme);
 
+    gtk_search_bar_connect_entry (self->search_bar,
+                                  GTK_EDITABLE (self->search_entry));
+
     app_iterator = pins_app_iterator_new_from_paths (pins_system_app_paths ());
 
     pins_app_list_set_app_iterator (self->app_list, app_iterator);
+    pins_app_list_set_app_iterator (self->search_list, app_iterator);
 }
