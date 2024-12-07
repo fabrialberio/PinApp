6a16258ea4f8c8233d215af4f2af03d060b26e86
fabrialberio <->
2024-12-07 14:00:09 +0100

    feature: Set PinsKeyRow text when set_key() is called

--- src/pins-key-row.c
+++ src/pins-key-row.c
@@ -23,6 +23,9 @@
 struct _PinsKeyRow
 {
     AdwEntryRow parent_instance;
+
+    PinsDesktopFile *desktop_file;
+    gchar *key;
 };
 
 G_DEFINE_TYPE (PinsKeyRow, pins_key_row, ADW_TYPE_ENTRY_ROW)
@@ -34,11 +37,29 @@ pins_key_row_new (void)
 }
 
 void
+pins_key_row_text_changed_cb (GtkEditable *self, PinsKeyRow *user_data)
+{
+    g_assert (PINS_IS_KEY_ROW (user_data));
+
+    pins_desktop_file_set_string (user_data->desktop_file, user_data->key,
+                                  gtk_editable_get_text (self));
+}
+
+void
 pins_key_row_set_key (PinsKeyRow *self, PinsDesktopFile *desktop_file,
                       gchar *key)
 {
-    /// TODO: Implement
+    self->desktop_file = desktop_file;
+    self->key = key;
+
+    gchar *value = pins_desktop_file_get_string (desktop_file, key, NULL);
+
     adw_preferences_row_set_title (ADW_PREFERENCES_ROW (self), key);
+    gtk_editable_set_text (GTK_EDITABLE (self), value);
+
+    /// TODO: Update text when desktop file changes
+    g_signal_connect (GTK_EDITABLE (self), "changed",
+                      G_CALLBACK (pins_key_row_text_changed_cb), self);
 }
 
 static void
