2e2debe78bd2fb6991cd71489ba451308ba9a43e
fabrialberio <->
2025-02-11 14:24:21 +0100

    feature: Implement "Show all apps" toggle

--- src/pins-app-view.c
+++ src/pins-app-view.c
@@ -31,8 +31,11 @@ struct _PinsAppView
 {
     AdwBin parent_instance;
 
-    GtkStringFilter *string_filter;
-    GtkFilterListModel *filter_model;
+    gboolean show_all_apps;
+    GtkCustomFilter *show_all_apps_filter;
+    GtkStringFilter *search_filter;
+    GtkFilterListModel *show_all_apps_filter_model;
+    GtkFilterListModel *search_filter_model;
 
     GtkToggleButton *search_button;
     GtkSearchBar *search_bar;
@@ -42,6 +45,13 @@ struct _PinsAppView
 };
 
 G_DEFINE_TYPE (PinsAppView, pins_app_view, ADW_TYPE_BIN);
+
+enum
+{
+    PROP_0,
+    PROP_SHOW_ALL_APPS,
+    N_PROPS,
+};
 
 enum
 {
@@ -57,6 +67,7 @@ enum
     N_PAGES,
 };
 
+static GParamSpec *properties[N_PROPS];
 static guint signals[N_SIGNALS];
 static gchar *pages[N_PAGES] = {
     "apps",
@@ -92,11 +103,12 @@ pins_app_view_set_app_iterator (PinsAppView *self,
     adw_view_stack_set_visible_child_name (self->view_stack,
                                            pages[PAGE_LOADING]);
 
-    self->filter_model = gtk_filter_list_model_new (
-        G_LIST_MODEL (app_iterator), GTK_FILTER (self->string_filter));
+    gtk_filter_list_model_set_model (
+        g_object_ref (self->show_all_apps_filter_model),
+        G_LIST_MODEL (app_iterator));
 
-    pins_app_grid_set_model (self->app_grid,
-                             G_LIST_MODEL (g_object_ref (self->filter_model)));
+    pins_app_grid_set_model (self->app_grid, G_LIST_MODEL (g_object_ref (
+                                                 self->search_filter_model)));
 
     g_signal_connect_object (app_iterator, "loading",
                              G_CALLBACK (app_iterator_loading_cb), self, 0);
@@ -110,12 +122,46 @@ pins_app_view_dispose (GObject *object)
 {
     PinsAppView *self = PINS_APP_VIEW (object);
 
-    g_clear_object (&self->string_filter);
-    g_clear_object (&self->filter_model);
+    g_clear_object (&self->search_filter);
+    g_clear_object (&self->show_all_apps_filter);
+    g_clear_object (&self->show_all_apps_filter_model);
+    g_clear_object (&self->search_filter_model);
 
     gtk_widget_dispose_template (GTK_WIDGET (object), PINS_TYPE_APP_VIEW);
 
     G_OBJECT_CLASS (pins_app_view_parent_class)->dispose (object);
+}
+
+static void
+pins_desktop_file_get_property (GObject *object, guint prop_id, GValue *value,
+                                GParamSpec *pspec)
+{
+    PinsAppView *self = PINS_APP_VIEW (object);
+
+    switch (prop_id)
+        {
+        case PROP_SHOW_ALL_APPS:
+            g_value_set_boolean (value, self->show_all_apps);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        }
+}
+
+static void
+pins_desktop_file_set_property (GObject *object, guint prop_id,
+                                const GValue *value, GParamSpec *pspec)
+{
+    PinsAppView *self = PINS_APP_VIEW (object);
+
+    switch (prop_id)
+        {
+        case PROP_SHOW_ALL_APPS:
+            self->show_all_apps = g_value_get_boolean (value);
+            break;
+        default:
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+        }
 }
 
 static void
@@ -125,6 +171,14 @@ pins_app_view_class_init (PinsAppViewClass *klass)
     GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
     object_class->dispose = pins_app_view_dispose;
+    object_class->get_property = pins_desktop_file_get_property;
+    object_class->set_property = pins_desktop_file_set_property;
+
+    properties[PROP_SHOW_ALL_APPS] = g_param_spec_boolean (
+        "show-all-apps", "Show All Apps", "Whether all apps are shown", FALSE,
+        G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+    g_object_class_install_properties (object_class, N_PROPS, properties);
 
     signals[ACTIVATE] = g_signal_new ("activate", G_TYPE_FROM_CLASS (klass),
                                       G_SIGNAL_RUN_LAST, 0, NULL, NULL, NULL,
@@ -151,9 +205,10 @@ pins_app_view_search_changed_cb (GtkSearchEntry *entry, PinsAppView *self)
     g_assert (PINS_IS_APP_VIEW (self));
 
     gtk_string_filter_set_search (
-        self->string_filter, gtk_editable_get_text (GTK_EDITABLE (entry)));
+        self->search_filter, gtk_editable_get_text (GTK_EDITABLE (entry)));
 
-    if (g_list_model_get_n_items (G_LIST_MODEL (self->filter_model)) == 0)
+    if (g_list_model_get_n_items (G_LIST_MODEL (self->search_filter_model))
+        == 0)
         adw_view_stack_set_visible_child_name (self->view_stack,
                                                pages[PAGE_EMPTY]);
     else
@@ -170,9 +225,34 @@ pins_app_view_item_activated_cb (GtkListView *self, guint position,
     g_assert (PINS_IS_APP_VIEW (user_data));
 
     desktop_file = g_list_model_get_item (
-        G_LIST_MODEL (user_data->filter_model), position);
+        G_LIST_MODEL (user_data->search_filter_model), position);
 
     g_signal_emit (user_data, signals[ACTIVATE], 0, desktop_file);
+}
+
+void
+show_all_apps_notify_cb (PinsAppView *self, GParamSpec *pspec)
+{
+    GtkFilterChange change = self->show_all_apps
+                                 ? GTK_FILTER_CHANGE_LESS_STRICT
+                                 : GTK_FILTER_CHANGE_MORE_STRICT;
+
+    gtk_filter_changed (GTK_FILTER (self->show_all_apps_filter), change);
+}
+
+gboolean
+show_all_filter_match_func (gpointer desktop_file, gpointer user_data)
+{
+    PinsAppView *self = PINS_APP_VIEW (user_data);
+
+    g_assert (PINS_IS_DESKTOP_FILE (desktop_file));
+
+    if (self->show_all_apps)
+        return TRUE;
+
+    return pins_desktop_file_is_shown (PINS_DESKTOP_FILE (desktop_file))
+           || pins_desktop_file_is_user_edited (
+               PINS_DESKTOP_FILE (desktop_file));
 }
 
 static void
@@ -192,8 +272,24 @@ pins_app_view_init (PinsAppView *self)
 
     gtk_widget_init_template (GTK_WIDGET (self));
 
-    self->string_filter = gtk_string_filter_new (gtk_property_expression_new (
+    g_settings_bind (settings, "show-all-apps", self, "show-all-apps",
+                     G_SETTINGS_BIND_DEFAULT);
+
+    g_signal_connect_object (self, "notify::show-all-apps",
+                             G_CALLBACK (show_all_apps_notify_cb), self, 0);
+
+    self->search_filter = gtk_string_filter_new (gtk_property_expression_new (
         PINS_TYPE_DESKTOP_FILE, NULL, "search-string"));
+
+    self->show_all_apps_filter
+        = gtk_custom_filter_new (&show_all_filter_match_func, self, NULL);
+
+    self->show_all_apps_filter_model = gtk_filter_list_model_new (
+        NULL, GTK_FILTER (self->show_all_apps_filter));
+
+    self->search_filter_model = gtk_filter_list_model_new (
+        G_LIST_MODEL (self->show_all_apps_filter_model),
+        GTK_FILTER (self->search_filter));
 
     adw_view_stack_set_visible_child_name (self->view_stack,
                                            pages[PAGE_LOADING]);
